dcd889f55a5006406ebd7e371b380d667a351320
Mythical-Github <MythicalData@gmail.com>
2023-10-02 02:15:54 -0400

    Initial Upload

---
+++ Source/EscapeTheBackrooms/EscapeTheBackroomsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheBackroomsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEBACKROOMS_API AEscapeTheBackroomsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
